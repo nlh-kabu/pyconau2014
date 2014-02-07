0436f1442bdea0bb7417ca0d74704e0e8e5af6eb
Nicole Harris <n.harris@kabucreative.com.au>
2014-02-07 17:34:34 +1100

    Homepage - footer, carousels, google map

--- js/main.js
+++ js/main.js
@@ -19,6 +19,48 @@ $(document).ready(function(){
     });
 
 
-});
+    // Enable Carousels
 
+    $("#speakers").owlCarousel({
+        items: 4
+    });
+
+    $("#sponsors").owlCarousel({
+        items: 6
+    });
+
+
+
+    // Embed Google Map
+
+    function initialize() {
+
+        var myLatLng = new google.maps.LatLng(-27.476341,153.018298);
+
+        var mapOptions = {
+            zoom: 14,
+            center: myLatLng
+        };
+
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+        var infowindow = new google.maps.InfoWindow({
+            content: 'Brisbane Convention & Exhibition Centre'
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Brisbane Convention & Exhibition Centre'
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+        });
+    }
+
+    google.maps.event.addDomListener(window, 'resize', initialize);
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+});
 
